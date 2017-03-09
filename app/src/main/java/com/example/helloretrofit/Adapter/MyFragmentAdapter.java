3d9a21968a566b214914bdfb7c79d2fbbff36ef7
ligen <798167096@qq.com>
2017-03-09 18:00:19 +0800

    修改了最后两页无法加载的问题

--- app/src/main/java/com/example/helloretrofit/Adapter/MyFragmentAdapter.java
+++ app/src/main/java/com/example/helloretrofit/Adapter/MyFragmentAdapter.java
@@ -19,7 +19,7 @@ public class MyFragmentAdapter extends FragmentPagerAdapter {
 
     @Override
     public Fragment getItem(int position) {
-        return PageFragment.newInstance(position + 1);
+        return PageFragment.newInstance(position);
     }
 
     @Override
